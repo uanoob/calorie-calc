51e4106562d602a61ce73b48a249fddb22082d17
uanoob <usnet@ukr.net>
2018-06-19 11:52:08 +0300

    Get & Populate Items

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ const ItemCtrl = (function() {
   };
   // Public Methods
   return {
+    getItems: function() {
+      return data.items;
+    },
     logData: function() {
       return data;
     },
@@ -28,8 +31,29 @@ const ItemCtrl = (function() {
 
 // UI Controller
 const UICtrl = (function() {
+  const UISelectors = {
+    itemList: '#item-list',
+  };
   // Public Methods
-  return {};
+  return {
+    populateItemList: function(items) {
+      let html = '';
+
+      items.forEach(function(item) {
+        html += `
+          <li class="collection-item" id="item-${item.id}">
+            <strong>${item.name}: </strong>
+            <em>${item.calories} Calories</em>
+            <a href="#" class="secondary-content">
+              <i class="edit-item fa fa-pencil"></i>
+            </a>
+          </li>
+        `;
+      });
+      // Insert list items
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    },
+  };
 })();
 
 // App Controller
@@ -37,7 +61,11 @@ const App = (function(ItemCtrl, UICtrl) {
   // Public Methods
   return {
     init: function() {
-      console.log('Initializing App...');
+      // Fetch items from data structure
+      const items = ItemCtrl.getItems();
+
+      // Populate list with items
+      UICtrl.populateItemList(items);
     },
   };
 })(ItemCtrl, UICtrl);
